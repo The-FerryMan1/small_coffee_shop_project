71cd23de6eeac4186bf792dc6487fc5d01a9e5d1
The-FerryMan1 <mercedroxy@gmail.com>
2025-10-24 15:58:00 +0800

    setup database,databse schema and added zod for valdiation

---
+++ coffee_shop_server/goal.markdown
@@ -0,0 +1,7 @@
+<!-- x means done -->
+<!-- Coffeeshop application -->
+[] 1. Setup database, table and schema
+[] 2. Authenticantion, JWT token 
+[] 3. Email verification 
+[] 4. Endpoint services
+
--- coffee_shop_server/src/server.ts
+++ coffee_shop_server/src/server.ts
@@ -1,11 +1,19 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
+
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.use(logger())
+
+app.get('/ping', (c) => {
+  return c.text('pong', 200)
 })
+
+
+
+
 
 serve({
   fetch: app.fetch,
